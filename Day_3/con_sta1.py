53ff800e8682f28e7acfe365217bc33a4c9120d3
Pratik Thakur <pratik.pt.thakur@gmail.com>
2026-04-06 03:52:34 -0700

    this conditionals are completed...
    going through the function in 1 hour it will be completed then.

---
+++ Day_3/con_sta1.py
@@ -0,0 +1,27 @@
+# in python language there are few conditional statements like:
+# if, if-else, if-elif, if-elif-else....
+
+a=100
+b=50
+if a>b:
+    print("true")
+elif a==b:
+    print("both are equals")
+else :
+    print("b is smaller then a")
+
+print("-----------------------------------------")
+
+
+a=5
+b=2
+if a<b: print("a is smaller then b")
+else: print("doesn't matter who is small or bigger")
+
+print("----------------------------------------------")
+
+
+a=303
+b=303
+print("a") if a>b else print("=")
+print
---
+++ Day_3/con_sta2.py
@@ -0,0 +1,20 @@
+# in python there are logical operations 
+# and = both of the conditions must be true.
+#  not = reverse the result, return false if the result is turn.
+
+a= 100
+b= 200
+if a>b and a==b:
+    print('a and b are valid words')
+else:
+    print("False")
+
+print("-----------------------")
+
+if a<b or a==b:
+    print("true")
+
+print("-------------------------------")
+
+if a is not b: print("the reverse of result")
+
